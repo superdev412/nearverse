1c5a724e50e4a451cc50b747897b527f8118b957
Super-Smile <baydev1998@gmail.com>
2022-03-31 11:45:05 +0300

    update

--- src/components/roadmap/roadmap.js
+++ src/components/roadmap/roadmap.js
@@ -1,15 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import HeaderGal from '../include/headerGal';
-import '../../assets/css/roadmap/roadmap.css'
-function Roadmap() {
-    return(
-        <div>
-            <HeaderGal />
-            <div className='iframe-part'>
-                <iframe src="https://apac-rm.roadmunk.com/publish/e6a7b7856b940c63a1de63cd6980d98dd5c59d89" className='roadmap-iframe' title="W3Schools Free Online Web Tutorials">
-                </iframe>
+import '../../assets/css/roadmap/roadmap.css';
+
+class Roadmap extends React.Component{ 
+    
+    render(){
+        return(
+            <div>
+                <HeaderGal />
+                <div className='iframe-part'>
+                    <iframe src="https://apac-rm.roadmunk.com/publish/e6a7b7856b940c63a1de63cd6980d98dd5c59d89" className='roadmap-iframe' title="W3Schools Free Online Web Tutorials">
+                    </iframe>
+                </div>
             </div>
-        </div>
-    )
-}
+        )}
+    }
 
 export default Roadmap;
